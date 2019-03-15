2ca3637ca6c1695545a091e3da40aeda8f3db912
venkatesanlalitha <venkatesanshivangi@gmail.com>
2019-03-15 22:41:45 +0000

    TubeDataChecker
    TubeDataChecker

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ Created on Tue Mar 12 22:29:59 2019
 """
 
 from flask import Flask
+import requests
 app = Flask(__name__)
 
 @app.route('/')
@@ -15,5 +16,13 @@ def home():
 @app.route('/Hello')
 def Hello():
     return "Oh boy"
+@app.route('/GetAllLineStatus')
+def GetAllLineStatus():
+    resp = requests.get('https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status?app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba')
+    for todo_item in resp.json():
+        trainName = todo_item['name']
+        modelName = todo_item['modeName']
+        statusDescription = todo_item['lineStatuses'][0]['statusSeverityDescription']
+    return "Done"
 if __name__ == "__main__":
     app.run()
