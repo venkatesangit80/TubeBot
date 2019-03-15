1b9974e3a472f2ab23aa227d2f370ae0df2d172e
venkatesanlalitha <venkatesanshivangi@gmail.com>
2019-03-15 22:53:32 +0000

    StatusData
    Status Data

--- app.py
+++ app.py
@@ -18,11 +18,13 @@ def Hello():
     return "Oh boy"
 @app.route('/GetAllLineStatus')
 def GetAllLineStatus():
+    responseValue = ""
     resp = requests.get('https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status?app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba')
     for todo_item in resp.json():
         trainName = todo_item['name']
         modelName = todo_item['modeName']
         statusDescription = todo_item['lineStatuses'][0]['statusSeverityDescription']
+        responseValue = " " + responseValue + " " + trainName + " " + modelName + " " + statusDescription
     return "Done"
 if __name__ == "__main__":
     app.run()
