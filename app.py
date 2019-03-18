47c2db5c50f4cbde0cdfcb6f23d0320bcfd7f7c9
venkatesanlalitha <venkatesanshivangi@gmail.com>
2019-03-18 20:43:40 +0000

    Status Implementation
    Status Implementation

--- app.py
+++ app.py
@@ -53,6 +53,14 @@ def GetAllLineStatus():
                     for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
                         response_text = response_text + singleAffected['commonName'] + ","  
 
+            if "Reduced" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
+                statusUrl = "https://api-argon.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
+                subResp = requests.get(statusUrl)
+                for sub_todo_item in subResp.json():
+                    response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
+                    for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
+                        response_text = response_text + singleAffected['commonName'] + ","  
+
         allLineNames = allLineNames + todo_item['name'] + ","
         responseText = responseText + "<tr>"
         trainLineDetails = {}
