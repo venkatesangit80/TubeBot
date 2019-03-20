46b6768e37dead99c9652df662f0153b77feb0b3
venkatesanlalitha <venkatesanshivangi@gmail.com>
2019-03-20 21:17:37 +0000

    Update app.py

--- app.py
+++ app.py
@@ -20,84 +20,77 @@ def Hello():
 def GetAllLineStatus():
     data = request.get_json(silent=True)
     inputValue = data['queryResult']['parameters']['input']
-    incommingAction = data['queryResult']['parameters']['input']
     response_text = ""
     reply = {}
-    if(incommingAction == "LineStatus"):
-        inputValue = inputValue.replace("and","&")
-        #input
-        responseValue = []
-        responseText = "<Table>"
-        allLineNames = ""
-        resp = requests.get('https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status?app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba')
-        for todo_item in resp.json():
-            if todo_item['name'].upper() == inputValue.upper():
-                response_text = response_text + " " + todo_item['name'] + " " + todo_item['lineStatuses'][0]['statusSeverityDescription']
-    
-                if "Part" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
-                    statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
-                    subResp = requests.get(statusUrl)
-                    for sub_todo_item in subResp.json():
-                        response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
-                        for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
-                            response_text = response_text + singleAffected['commonName'] + ","
-            
-                if "Closed" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
-                    statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
-                    subResp = requests.get(statusUrl)
-                    for sub_todo_item in subResp.json():
-                        response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
-                        for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
-                            response_text = response_text + singleAffected['commonName'] + ","        
-            
-                if "Minor" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
-                    statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
-                    subResp = requests.get(statusUrl)
-                    for sub_todo_item in subResp.json():
-                        response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
-                        for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
-                            response_text = response_text + singleAffected['commonName'] + ","      
-    
-            
-                if "Severe" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
-                    statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
-                    subResp = requests.get(statusUrl)
-                    for sub_todo_item in subResp.json():
-                        response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
-                        for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
-                            response_text = response_text + singleAffected['commonName'] + ","   
-    
-                if "Reduced" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
-                    statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
-                    subResp = requests.get(statusUrl)
-                    for sub_todo_item in subResp.json():
-                        response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
-                        for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
-                            response_text = response_text + singleAffected['commonName'] + ","      
-    
-            allLineNames = allLineNames + todo_item['name'] + ","
-            responseText = responseText + "<tr>"
-            trainLineDetails = {}
-            trainLineDetails['LineName'] = todo_item['name']
-            responseText = responseText + "<td>" + todo_item['name'] + "</td>"
-            trainLineDetails['ModelName'] = todo_item['modeName']
-            responseText = responseText + "<td>" + todo_item['modeName'] + "</td>"
-            trainLineDetails['StatusDescription'] = todo_item['lineStatuses'][0]['statusSeverityDescription']
-            responseText = responseText + "<td>" + todo_item['lineStatuses'][0]['statusSeverityDescription'] + "</td>"
-            responseValue.append(trainLineDetails)
-            responseText = responseText + "</tr>"
-            #response_text = response_text + " Line " + todo_item['name'] + " Status Is - " + todo_item['lineStatuses'][0]['statusSeverityDescription'] + " , "
-        responseText = responseText + "</table>"
-        if response_text == "":
-            response_text = "Line " + inputValue + " Not Found. The available lines are : " + allLineNames
-        reply = {
-            "fulfillmentText" : response_text
-        }
-    if(incommingAction == "BikePoints"):
-        response_text = GetBikePointDetails(inputValue)
-        reply = {
-            "fulfillmentText" : response_text
-        }
+    inputValue = inputValue.replace("and","&")
+    #input
+    responseValue = []
+    responseText = "<Table>"
+    allLineNames = ""
+    resp = requests.get('https://api.tfl.gov.uk/line/mode/tube,overground,dlr,tflrail/status?app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba')
+    for todo_item in resp.json():
+        if todo_item['name'].upper() == inputValue.upper():
+            response_text = response_text + " " + todo_item['name'] + " " + todo_item['lineStatuses'][0]['statusSeverityDescription']
+
+            if "Part" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
+                statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
+                subResp = requests.get(statusUrl)
+                for sub_todo_item in subResp.json():
+                    response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
+                    for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
+                        response_text = response_text + singleAffected['commonName'] + ","
+        
+            if "Closed" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
+                statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
+                subResp = requests.get(statusUrl)
+                for sub_todo_item in subResp.json():
+                    response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
+                    for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
+                        response_text = response_text + singleAffected['commonName'] + ","        
+        
+            if "Minor" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
+                statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
+                subResp = requests.get(statusUrl)
+                for sub_todo_item in subResp.json():
+                    response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
+                    for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
+                        response_text = response_text + singleAffected['commonName'] + ","      
+
+        
+            if "Severe" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
+                statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
+                subResp = requests.get(statusUrl)
+                for sub_todo_item in subResp.json():
+                    response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
+                    for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
+                        response_text = response_text + singleAffected['commonName'] + ","   
+
+            if "Reduced" in todo_item['lineStatuses'][0]['statusSeverityDescription']:
+                statusUrl = "https://api.tfl.gov.uk/Line/" + todo_item['name'].replace(" ", "-") + "/Status?detail=true&app_id=bd38b189&app_key=307678e9c079a6c525da5304098522ba"
+                subResp = requests.get(statusUrl)
+                for sub_todo_item in subResp.json():
+                    response_text = response_text + " --- " + sub_todo_item['lineStatuses'][0]['reason'] + " ---- FOR ---- "
+                    for singleAffected in sub_todo_item['lineStatuses'][0]['disruption']['affectedStops']:
+                        response_text = response_text + singleAffected['commonName'] + ","      
+
+        allLineNames = allLineNames + todo_item['name'] + ","
+        responseText = responseText + "<tr>"
+        trainLineDetails = {}
+        trainLineDetails['LineName'] = todo_item['name']
+        responseText = responseText + "<td>" + todo_item['name'] + "</td>"
+        trainLineDetails['ModelName'] = todo_item['modeName']
+        responseText = responseText + "<td>" + todo_item['modeName'] + "</td>"
+        trainLineDetails['StatusDescription'] = todo_item['lineStatuses'][0]['statusSeverityDescription']
+        responseText = responseText + "<td>" + todo_item['lineStatuses'][0]['statusSeverityDescription'] + "</td>"
+        responseValue.append(trainLineDetails)
+        responseText = responseText + "</tr>"
+        #response_text = response_text + " Line " + todo_item['name'] + " Status Is - " + todo_item['lineStatuses'][0]['statusSeverityDescription'] + " , "
+    responseText = responseText + "</table>"
+    if response_text == "":
+        response_text = "Line " + inputValue + " Not Found. The available lines are : " + allLineNames
+    reply = {
+        "fulfillmentText" : response_text
+    }
         
     return jsonify(reply)
 
