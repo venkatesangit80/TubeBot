fe278e00e83aac88f6df8c1ae314cdfb95a43fc9
venkatesanlalitha <venkatesanshivangi@gmail.com>
2019-03-24 00:22:51 +0000

    With Cards
    With Cards 1

--- app.py
+++ app.py
@@ -98,12 +98,12 @@ def GetAllLineStatus():
         inputValue = inputValue.replace("&","-")
         inputValue = inputValue.replace(" ","")
         subInputValue = data['queryResult']['parameters']['subinput']
-        response_text = GetCurrentSpot(inputValue,subInputValue)
-        #response_text = GetCurrentSpotCard(inputValue,subInputValue)
+        #response_text = GetCurrentSpot(inputValue,subInputValue)
+        response_text = GetCurrentSpotCard(inputValue,subInputValue)
         if(response_text == ""):
             response_text = "No Prediction for the " + subInputValue
         reply = {
-                "fulfillmentText" : response_text
+                "fulfillmentMessages" : response_text
                 }
         return jsonify(reply)
     if(action == "TestRich"):
